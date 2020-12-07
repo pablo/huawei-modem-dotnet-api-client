b243280186322555cb39b2914a75af485ae34e19
psantacr <pablo@roshka.com.py>
2020-12-06 23:32:46 -0300

    Add project files.

---
+++ api.cs
@@ -0,0 +1,41 @@
+﻿using huaweisms.data;
+
+namespace huaweisms.api
+{
+
+    public class User
+    {
+
+        public ApiCtx QuickLogin(ApiCtx ctx, string userName, string password)
+        {
+
+
+
+        }
+
+    }
+
+    public class SMS
+    {
+
+        public void SendSMS(ApiCtx apiCtx)
+        {
+
+        }
+
+    }
+
+    public class WebServer
+    {
+        public string GetSessionTokenInfo(ApiCtx ctx)
+        {
+
+            var apiResponse = ctx.HttpGet($"{ctx.Config.BaseURL}/webserver/SesTokInfo");
+
+
+            return ";";
+
+        }
+    }
+
+}
---
+++ data.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Xml;
+
+namespace huaweisms.data {
+
+    public class ApiConfig
+    {
+
+        public ApiConfig()
+        {
+            this.BaseURL = "http://192.168.8.1/api";
+            this.HttpTimeout = 10000;
+        }
+
+        public string BaseURL
+        {
+            get; set;
+        }
+
+        public int HttpTimeout
+        {
+            get; set;
+        }
+
+    }
+
+    public class ApiResponse
+    {
+
+        public enum ApiResponseType
+        {
+            XML,
+            TEXT,
+            JSON,
+            HTML,
+            UNDEFINED
+        }
+
+
+        private ApiResponse()
+        {
+            Response = new Dictionary<string, object>();
+        }
+
+        public Dictionary<string, object> Response
+        {
+            get; private set;
+        }
+
+        public ApiResponseType Type
+        {
+            get; private set;
+        }
+
+        public ContentType ContentType
+        {
+            get; private set;
+        }
+
+        private void ParseXmlResponse(string xml)
+        {
+            Response["data"] = null;
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+
+            foreach(XmlNode node in xmlDocument.DocumentElement.ChildNodes)
+            {
+                if (node.NodeType == XmlNodeType.Element)
+                {
+
+                }
+
+
+            }
+
+
+        }
+
+        private void ParseTextResponse(string text)
+        {
+
+            Response["data"] = text;
+        }
+
+        public static ApiResponse ReadAndCreateApiResponse(HttpResponseMessage response)
+        {
+            ApiResponse ret = new ApiResponse();
+
+            IEnumerable<string> contentTypes;
+            if (response.Headers.TryGetValues("Content-Type", out contentTypes))
+            {
+                var contentType = new ContentType(contentTypes.First());
+                ret.ContentType = contentType;
+                if (contentType.MediaType.Contains("text/xml"))
+                {
+                    ret.Type = ApiResponseType.XML;
+                }
+                else if (contentType.MediaType.Contains("text/plain"))
+                {
+                    ret.Type = ApiResponseType.TEXT;
+                }
+                else if (contentType.MediaType.Contains("text/html"))
+                {
+                    ret.Type = ApiResponseType.HTML;
+                }
+                else if (contentType.MediaType.Contains("application/json"))
+                {
+                    ret.Type = ApiResponseType.JSON;
+                }
+            } else
+            {
+                // if Content-Type not set, assume XML
+                ret.ContentType = new ContentType("text/xml; charset=utf-8");
+                ret.Type = ApiResponseType.XML;
+            }
+
+            switch (ret.Type)
+            {
+                case ApiResponseType.XML:
+                    ret.ParseXmlResponse(response.Content.ReadAsStringAsync().Result);
+                    break;
+                case ApiResponseType.TEXT:
+                    ret.ParseTextResponse(response.Content.ReadAsStringAsync().Result);
+                    break;
+                default:
+                    throw new Exception("Response Type: " + ret.Type + " currently not supported";
+            }
+
+            return ret;
+        }
+
+        public ApiResponse(HttpContent httpContent)
+        {
+
+        }
+
+
+    }
+
+    public class ApiCtx
+    {
+
+        public ApiCtx(ApiConfig config)
+        {
+            this.Config = config;
+            HttpClient = new HttpClient();
+            HttpClient.Timeout = TimeSpan.FromMilliseconds(config.HttpTimeout);
+        }
+
+        public ApiConfig Config
+        {
+            get; private set;
+        }
+
+        public HttpClient HttpClient
+        {
+            get; private set;
+        }
+
+        public string SessionId
+        {
+            get; set;
+        }
+
+        public string LoginToken
+        {
+            get; set;
+        }
+
+        public bool LoggedIn
+        {
+            get; set;
+        }
+
+        public ApiResponse HttpGet(String url)
+        {
+            var response = HttpClient.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return ApiResponse.ReadAndCreateApiResponse(response);
+            }
+            throw new Exception("HTTP request failed: " + response.ReasonPhrase);
+        }
+
+    }
+
+}
